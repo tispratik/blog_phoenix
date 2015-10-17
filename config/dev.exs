849a92bb975196b9e790c682be28c822b092613e
Pratik Khadloya <tispratik@gmail.com>
2015-10-17 08:22:01 -0700

    Add phoenix_haml

--- config/dev.exs
+++ config/dev.exs
@@ -20,7 +20,8 @@ config :blog, Blog.Endpoint,
     patterns: [
       ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$},
       ~r{web/views/.*(ex)$},
-      ~r{web/templates/.*(eex)$}
+      ~r{web/templates/.*(eex)$},
+      ~r{web/templates/.*(eex|haml)$}
     ]
   ]
 
