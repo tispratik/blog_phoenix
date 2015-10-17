ef4b338a656f153a2da7b3b04597ccbd8319afb7
Pratik Khadloya <tispratik@gmail.com>
2015-10-17 06:33:46 -0700

    Fix syntax error for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -13,9 +13,9 @@ use Mix.Config
 # which you typically run after static files are built.
 config :blog, Blog.Endpoint,
   http: [port: System.get_env("PORT")],
-  url: [scheme: "https", host: "fathomless-river-7820.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "shielded-forest-1149.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
